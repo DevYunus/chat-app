908a234ff290d2177964327965e8a2999dd64451
Yunus Shaikh <yunus.work829@gmail.com>
2018-09-23 15:17:29 +0530

    Socket connected using laravel node and laravel events-broadcast

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Events\MessageSent;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        event(new MessageSent());
         return view('home');
     }
 }
