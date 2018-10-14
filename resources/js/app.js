905718af2a996b77bf7d2387ca41c8aa42e0947a
Yunus Shaikh <yunus.shaikh@baxture.com>
2018-10-14 12:26:46 +0530

    finish up

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@
 // import VueSocketio from 'vue-socket.io';
 import io from 'socket.io-client';
 
-const socket = io('https://localhost:8890');
+const socket = io('http://localhost:8890');
 
 socket.on('connect', function(){
     console.log('connected on local');
